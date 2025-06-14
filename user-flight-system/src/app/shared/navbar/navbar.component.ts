61e98d76621e2afa2762343f2f60346892474338
ghadir <ghadirfakih164@gmail.com>
2025-06-14 08:40:45 +0300

    navbar logout

--- user-flight-system/src/app/shared/navbar/navbar.component.ts
+++ user-flight-system/src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
   imports: [CommonModule, RouterModule],
@@ -8,5 +10,18 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
 
+  logout(): void {
+    // Clear session data (e.g., token from localStorage/sessionStorage)
+    localStorage.removeItem('token');
+    sessionStorage.clear();
+
+    // Redirect to login page
+    this.router.navigate(['/login']);
+  }
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+  
 }
